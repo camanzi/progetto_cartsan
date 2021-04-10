d97b8d2db2fd1c10806dc01303810aafc5f2c249
Lorenzo Camanzi <lorenzo.camanzi@gmail.com>
2021-04-10 16:51:22 +0200

    Capito meglio come funziona DAO

---
+++ src/cartsan_camanzi/model/Cliente.java
@@ -0,0 +1,161 @@
+package cartsan_camanzi.model;
+
+public class Cliente {
+
+	private int idCliente;
+	private String denominazioneCliente;
+	private String codiceCliente;
+	private String tipologiaCliente;
+	private String ragioneSocialeCliente;
+	private String codiceFiscaleCliente;
+	private String partitaIvaCliente;
+	private String statoCliente;
+	private String dataAttivazioneCliente;
+	private String noteCliente;
+	private String emailFattura; 
+	private String citta1Cliente;
+	private String citta2Cliente;
+	private String cap1Cliente;
+	private String cap2Cliente;
+	private String via1Cliente;
+	private String via2Cliente;
+	private int eliminato;         
+	private String dataInserimentoCliente;
+	private int cloudCartsan;
+	
+	/****************************************************************
+	 * 
+	 * GETTER E SETTER PER OGNI CAMPO
+	 * 
+	 *
+	 *****************************************************************/
+	
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getDenominazioneCliente() {
+		return denominazioneCliente;
+	}
+	public void setDenominazioneCliente(String denominazioneCliente) {
+		this.denominazioneCliente = denominazioneCliente;
+	}
+	public String getCodiceCliente() {
+		return codiceCliente;
+	}
+	public void setCodiceCliente(String codiceCliente) {
+		this.codiceCliente = codiceCliente;
+	}
+	public String getTipologiaCliente() {
+		return tipologiaCliente;
+	}
+	public void setTipologiaCliente(String tipologiaCliente) {
+		this.tipologiaCliente = tipologiaCliente;
+	}
+	public String getRagioneSocialeCliente() {
+		return ragioneSocialeCliente;
+	}
+	public void setRagioneSocialeCliente(String ragioneSocialeCliente) {
+		this.ragioneSocialeCliente = ragioneSocialeCliente;
+	}
+	public String getCodiceFiscaleCliente() {
+		return codiceFiscaleCliente;
+	}
+	public void setCodiceFiscaleCliente(String codiceFiscaleCliente) {
+		this.codiceFiscaleCliente = codiceFiscaleCliente;
+	}
+	public String getPartitaIvaCliente() {
+		return partitaIvaCliente;
+	}
+	public void setPartitaIvaCliente(String partitaIvaCliente) {
+		this.partitaIvaCliente = partitaIvaCliente;
+	}
+	public String getStatoCliente() {
+		return statoCliente;
+	}
+	public void setStatoCliente(String statoCliente) {
+		this.statoCliente = statoCliente;
+	}
+	public String getDataAttivazioneCliente() {
+		return dataAttivazioneCliente;
+	}
+	public void setDataAttivazioneCliente(String dataAttivazioneCliente) {
+		this.dataAttivazioneCliente = dataAttivazioneCliente;
+	}
+	public String getNoteCliente() {
+		return noteCliente;
+	}
+	public void setNoteCliente(String noteCliente) {
+		this.noteCliente = noteCliente;
+	}
+	public String getEmailFattura() {
+		return emailFattura;
+	}
+	public void setEmailFattura(String emailFattura) {
+		this.emailFattura = emailFattura;
+	}
+	public String getCitta1Cliente() {
+		return citta1Cliente;
+	}
+	public void setCitta1Cliente(String citta1Cliente) {
+		this.citta1Cliente = citta1Cliente;
+	}
+	public String getCitta2Cliente() {
+		return citta2Cliente;
+	}
+	public void setCitta2Cliente(String citta2Cliente) {
+		this.citta2Cliente = citta2Cliente;
+	}
+	public String getCap1Cliente() {
+		return cap1Cliente;
+	}
+	public void setCap1Cliente(String cap1Cliente) {
+		this.cap1Cliente = cap1Cliente;
+	}
+	public String getCap2Cliente() {
+		return cap2Cliente;
+	}
+	public void setCap2Cliente(String cap2Cliente) {
+		this.cap2Cliente = cap2Cliente;
+	}
+	public String getVia1Cliente() {
+		return via1Cliente;
+	}
+	public void setVia1Cliente(String via1Cliente) {
+		this.via1Cliente = via1Cliente;
+	}
+	public String getVia2Cliente() {
+		return via2Cliente;
+	}
+	public void setVia2Cliente(String via2Cliente) {
+		this.via2Cliente = via2Cliente;
+	}
+	public int getEliminato() {
+		return eliminato;
+	}
+	public void setEliminato(int eliminato) {
+		this.eliminato = eliminato;
+	}
+	public String getDataInserimentoCliente() {
+		return dataInserimentoCliente;
+	}
+	public void setDataInserimentoCliente(String dataInserimentoCliente) {
+		this.dataInserimentoCliente = dataInserimentoCliente;
+	}
+	public int getCloudCartsan() {
+		return cloudCartsan;
+	}
+	public void setCloudCartsan(int cloudCartsan) {
+		this.cloudCartsan = cloudCartsan;
+	}
+
+	/****************************************************************
+	 * 
+	 * FINE GETTER E SETTER PER OGNI CAMPO
+	 * 
+	 *
+	 *****************************************************************/
+
+}
