732d2faa3d2d744461ed71a449ba45a317dd2d9e
Lorenzo Camanzi <lorenzo.camanzi@gmail.com>
2021-04-15 17:32:07 +0200

    Aggiunta bozza di Javadoc

--- src/db/dao/ClienteDAO.java
+++ src/db/dao/ClienteDAO.java
@@ -6,8 +6,27 @@ import model.Cliente;
 
 public interface ClienteDAO {
 	
+	/*
+	 * @return la lista completa di clienti con affiancati il numero di ticket
+	 * */
+	
 	public List<Cliente> quantitaTicketPerCliente(Connection dbConn);
+	
+	/*
+	 * @return i 10 clienti con il maggior numero di ticket
+	 * */
+	
 	public List<Cliente> topDieciClientiPerTicket(Connection dbConn);
+	
+	/*
+	 * @return la lista completa di clienti con affiancati i minuti totali
+	 * */
+	
 	public List<Cliente> quantitaMinutiPerCliente(Connection dbConn);
+	
+	/*
+	 * @return i 10 clienti con il maggior numero di minuti
+	 * */
+	
 	public List<Cliente> topDieciMinutiPerCliente(Connection dbConn);
 }
--- src/db/dao/TicketDAO.java
+++ src/db/dao/TicketDAO.java
@@ -6,6 +6,13 @@ import model.Ticket;
 
 public interface TicketDAO {
 	
+	/*
+	 * @return una lista con mese e minuti
+	 * i minuti rappresentano quanti minuti ha occupato  il cliente
+	 * il mese è il mese a cui fanno riferimento i minuti
+	 * 
+	 * */
+	
 	public List<Ticket> ticketPerMesePerCliente(Connection dbConn, int idTicket);
 
 }
